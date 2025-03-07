453d80b31a3fbc826e1d01215142764b4e74c1fe
shoaib Malik <90180244+shoaib-malik-org@users.noreply.github.com>
2025-03-07 22:54:25 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Navbar } from '@/components/shaheen bagh/navbar'
+
+export default function Home() {
+  return (
+      <section>
+        <Navbar />
+        <div className='container'>
+          
+        </div>
+      </section>
+  )
+}
