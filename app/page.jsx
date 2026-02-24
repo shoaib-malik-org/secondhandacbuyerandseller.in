49147f065edca0068f8dc970858ea51248d2b15b
shoaib Malik <90180244+shoaib-malik-org@users.noreply.github.com>
2026-02-24 10:27:19 +0530

    schema added

--- app/page.jsx
+++ app/page.jsx
@@ -8,11 +8,46 @@ import { Gallery } from '@/components/gallery'
 import { Carousel } from '@/components/testimonial'
 import { Contact } from '@/components/contact'
 import { Footer } from '@/components/footer'
+import Script from 'next/script'
 
 export default function Home() {
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "Eco second hand ac sale purchase",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": " Shop no 1, near dairy farm, Pocket C, Sarita Vihar",
+      "addressLocality": "New Delhi",
+      "addressRegion": "DL",
+      "postalCode": "110076"
+    },
+    // "image": storeFront.src,
+    "email": "saritasecondhandac@gmail.com",
+    "telePhone": "+919211487421",
+    "openingHours": "Mo,Tu,We,Th,Fr,Sa,Su 09:00-21:00",
+    "openingHoursSpecification": [{
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "09:00",
+      "closes": "21:00"
+    }],
+
+  }
+
   return (
     <>
-      <Navbar  />
+      <Script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      <Navbar />
       <div className="container-fluid px-0" style={{ marginTop: "90px" }}>
         {/* main */}
         <main>
